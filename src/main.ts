d3d4c1427d8d9d09cafb9cc0800c387f69b5fcdf
jatinkumar-me <jatin208015@gmail.com>
2024-01-11 01:36:23 +0530

    close modal when clicked outside
    modified prototype for HTMLDialogElement

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import projects from './projects';
 
+// Modify the prototype to close modal when clicked outside
+HTMLDialogElement.prototype.triggerShow = HTMLDialogElement.prototype.showModal;
+
+HTMLDialogElement.prototype.showModal = function() {
+    this.triggerShow();
+    this.onclick = (event: MouseEvent) => {
+        let rect = this.getBoundingClientRect();
+        if (event.clientY < rect.top || event.clientY > rect.bottom) return this.close();
+        if (event.clientX < rect.left || event.clientX > rect.right) return this.close();
+    };
+};
+
 projects();
